fcd65d56dacb2dca8c4a3f8d2c9d341364a91edc
Emanuel Cuccato <emanuelcuccato@gmail.com>
2022-12-27 11:38:51 -0300

    2022-12-27_11.38

--- git add commit push.bat
+++
@@ -1,7 +0,0 @@
-FOR /f "usebackq" %%i IN (`PowerShell ^(Get-Date^).ToString^('yyyy-MM-dd_HH.mm'^)`) DO SET now=%%i
-git add .
-git commit -m "%now%"
-git push
-::git branch -M main
-::git push --set-upstream origin main
-PAUSE
